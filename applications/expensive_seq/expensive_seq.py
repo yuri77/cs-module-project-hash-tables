81d69cb226f4505fb3ccfb62d4091ab3a09b9b47
yurika sakae <yurika.sun@gmail.com>
2020-07-16 22:50:39 -0700

    expensive_seq complete

--- applications/expensive_seq/expensive_seq.py
+++ applications/expensive_seq/expensive_seq.py
@@ -1,9 +1,17 @@
 # Your code here
 
+cache = {}
+
 
 def expensive_seq(x, y, z):
-    # Your code here
+    key = (x, y, z)
 
+    if x <= 0:
+        return y+z
+    if key not in cache:
+        cache[key] = expensive_seq(
+            x-1, y+1, z)+expensive_seq(x-2, y+2, z*2)+expensive_seq(x-3, y+3, z*3)
+    return cache[key]
 
 
 if __name__ == "__main__":
