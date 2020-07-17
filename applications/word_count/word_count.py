675ca40741e3738d9585898ab1bd447e3c6322e8
yurika sakae <yurika.sun@gmail.com>
2020-07-16 23:33:56 -0700

    word count logic complete

--- applications/word_count/word_count.py
+++ applications/word_count/word_count.py
@@ -1,10 +1,35 @@
-def word_count(s):
-    # Your code here
+import re
 
+specials = ['"', ':', ';', ',', '.', '-', '+', '=', '/',
+            '\\', '|', '[', ']', '{', '}', '(', ')', '*', '^', '&']
+
+
+def replace_specials(string, specials):
+    for char in specials:
+        if char in string:
+            string = string.replace(char, "")
+    return string
+
+
+def word_count(s):
+    words_count = {}
+    # Your code here
+    # res = re.split('[/s:;,.-+=/\|[]/{/}()*^&]', s)
+    s = replace_specials(s, specials)
+    s = s.split()
+    print(s)
+    for word in s:
+        word = word.lower()
+        if word not in words_count:
+            words_count[word] = 1
+        else:
+            words_count[word] += 1
+    return words_count
 
 
 if __name__ == "__main__":
     print(word_count(""))
     print(word_count("Hello"))
     print(word_count('Hello, my cat. And my cat doesn\'t say "hello" back.'))
-    print(word_count('This is a test of the emergency broadcast network. This is only a test.'))
+    print(word_count(
+        'This is a test of the emergency broadcast network. This is only a test.'))
